d5cca9029edcac81d740d67befe711717bdceb71
ekwator <ekwator@mail.ru>
2025-02-10 10:03:23 +0500

    Add test for itsArrayOf и itsTypeOf

--- itsArrayOf.test.js
+++ itsArrayOf.test.js
@@ -1,1 +1,13 @@
-const { isMixedArray, isObjectArray, itSsparseArrNumber, itsArrNotNull, itsArrNul, itsArrayBuffer, itsMap, itsNestedArr, itsNumberArray, itsSet, itsSparseArrNotNull, itsSparseArrNull, itsStringArray, itsTypedArray } = require
+const { isMixedArray, isObjectArray, itSsparseArrNumber, itsArrNotNull, itsArrNul, itsArrayBuffer, itsMap, itsNestedArr, itsNumberArray, itsSet, itsSparseArrNotNull, itsSparseArrNull, itsStringArray, itsTypedArray } = require('./itsArrayOf');
+
+const { isMixedArray, isObjectArray } = require('./itsArrayOf');
+
+describe('Проверка функций для массивов', () => {
+  test('isMixedArray должен вернуть true для [1, "a", true, null]', () => {
+    expect(isMixedArray([1, 'a', true, null])).toBe(true);
+  });
+
+  test('isObjectArray должен вернуть false для [1, 2, 3]', () => {
+    expect(isObjectArray([1, 2, 3])).toBe(false);
+  });
+});
--- itsTypeOf.test.js
+++ itsTypeOf.test.js
@@ -1,1 +1,9 @@
-const { itsArrowFunc,  itsAsyncFunc, itsBigInt, itsBool, itsClass, itsClassExtends, itsClassGetter, itsClassMethod, itsClassSetter, itsClassStaticMethod, itsConstructorFunc, itsDate,itsError, itsFiniteNumber, itsGeneratorFunc, itsInfinity, itsNaN, itsObjNotNull, itsObjNull, itsObjNullNull, itsPrimitive, itsRegExp, itsRegularFunc, itsStringNotNull, itsStringNull, itsSymbolNotNull, itsSymbolNull, itsnewNumber,  itsnewUndef } = require
+const { itsArrowFunc, itsAsyncFunc, itsBigInt, itsBool, itsClass, itsClassExtends, itsClassGetter, itsClassMethod, itsClassSetter, itsClassStaticMethod, itsConstructorFunc, itsDate, itsError, itsFiniteNumber, itsGeneratorFunc, itsInfinity, itsNaN, itsObjNotNull, itsObjNull, itsObjNullNull, itsPrimitive, itsRegExp, itsRegularFunc, itsStringNotNull, itsStringNull, itsSymbolNotNull, itsSymbolNull, itsnewNumber, itsnewUndef } = require('./itsTypeOf');
+
+test('itsBool should return true for boolean true', () => {
+    expect(itsBool(true)).toBe(true);
+});
+
+test('itsBool should return false for a number', () => {
+    expect(itsBool(123)).toBe(false);
+});
